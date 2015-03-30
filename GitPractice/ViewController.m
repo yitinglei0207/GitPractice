2c68aa766e9ac5108fc3f1981b81f3303ea6036b
yitinglei <yitinglei0207@gmail.com>
2015-03-30 16:49:25 +0800

    zzzzzzz

--- GitPractice/ViewController.m
+++ GitPractice/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"cannot commit");
+    NSLog(@"cannot commitz zzzzzz");
     
 }
 
